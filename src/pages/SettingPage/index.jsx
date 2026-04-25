a0014f8f3e4d1f9a42f152f670de971754bb8fab
VCach333 <vladmircach333@gmail.com>
2026-04-25 22:56:41 +0100

    setting theme started

--- src/pages/SettingPage/index.jsx
+++ src/pages/SettingPage/index.jsx
@@ -1,5 +1,8 @@
 /* hooks & utils import */
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
+
+/* style import */
+import styles from './style.module'
 
 /* component import */
 import Header from '../../components/Layouts/Header'
@@ -10,8 +13,36 @@ export default function SettingPage() {
     <>
       <Header pageTitle='Ajustes' />
 
-      <View>
-        <Text>Ajustes</Text>
+      <View style={styles.settingPage}>
+        
+        <View style={styles.themeContent}>
+          
+          <View style={styles.themeContentItem}>
+            <Image
+              source={require('../../../assets/icons/setting.png')}
+              style={styles.themeIcon}
+            />
+            <Text style={styles.themeText}>Sistema</Text>
+          </View>
+
+          <View style={styles.themeContentItem}>
+            <Image
+              source={require('../../../assets/icons/setting.png')}
+              style={styles.themeIcon}
+            />
+            <Text style={styles.themeText}>Claro</Text>
+          </View>
+
+          <View style={styles.themeContentItem}>
+            <Image
+              source={require('../../../assets/icons/setting.png')}
+              style={styles.themeIcon}
+            />
+            <Text style={styles.themeText}>Escuro</Text>
+          </View>
+
+        </View>
+
       </View>
     </>
   )
